29993429b517e051c5dce70a4e66ead1bebef23c
AbuIbrahim <AntonRoumelR>
2025-03-02 17:34:16 +0800

    Added harakat letteres from Alif until Ya

---
+++ src/letters/harakat-letters.js
@@ -0,0 +1,422 @@
+export const shortVowels = [
+    {
+        letter: "اَ",
+        name: "Alif with Fathah",
+        pronunciation: ["a"],
+    },
+    {
+        letter: "اِ",
+        name: "Alif with Kasrah",
+        pronunciation: ["i"],
+    },
+    {
+        letter: "اُ",
+        name: "Alif with Dammah",
+        pronunciation: ["u"],
+    },
+    {
+        letter: "بَ",
+        name: "Ba with Fathah",
+        pronunciation: ["ba"],
+    },
+    {
+        letter: "بِ",
+        name: "Ba with Kasrah",
+        pronunciation: ["bi"],
+    },
+    {
+        letter: "بُ",
+        name: "Ba with Dammah",
+        pronunciation: ["bu"],
+    },
+    {
+        letter: "تَ",
+        name: "Ta with Fathah",
+        pronunciation: ["ta"],
+    },
+    {
+        letter: "تِ",
+        name: "Ta with Kasrah",
+        pronunciation: ["ti"],
+    },
+    {
+        letter: "تُ",
+        name: "Ta with Dammah",
+        pronunciation: ["tu"],
+    },
+    {
+        letter: "ثَ",
+        name: "Tha with Fathah",
+        pronunciation: ["tha"],
+    },
+    {
+        letter: "ثِ",
+        name: "Tha with Kasrah",
+        pronunciation: ["thi"],
+    },
+    {
+        letter: "ثُ",
+        name: "Tha with Dammah",
+        pronunciation: ["thu"],
+    },
+    {
+        letter: "جَ",
+        name: "Jeem with Fathah",
+        pronunciation: ["ja"],
+    },
+    {
+        letter: "جِ",
+        name: "Jeem with Kasrah",
+        pronunciation: ["ji"],
+    },
+    {
+        letter: "جُ",
+        name: "Jeem with Dammah",
+        pronunciation: ["ju"],
+    },
+    {
+        letter: "حَ",
+        name: "Hah with Fathah",
+        pronunciation: ["ha"],
+    },
+    {
+        letter: "حِ",
+        name: "Hah with Kasrah",
+        pronunciation: ["hi"],
+    },
+    {
+        letter: "حُ",
+        name: "Hah with Dammah",
+        pronunciation: ["hu"],
+    },
+    {
+        letter: "خَ",
+        name: "Kha with Fathah",
+        pronunciation: ["kha"],
+    },
+    {
+        letter: "خِ",
+        name: "Kha with Kasrah",
+        pronunciation: ["khi"],
+    },
+    {
+        letter: "خُ",
+        name: "Kha with Dammah",
+        pronunciation: ["khu"],
+    },
+    {
+        letter: "دَ",
+        name: "Dal with Fathah",
+        pronunciation: ["da"],
+    },
+    {
+        letter: "دِ",
+        name: "Dal with Kasrah",
+        pronunciation: ["di"],
+    },
+    {
+        letter: "دُ",
+        name: "Dal with Dammah",
+        pronunciation: ["du"],
+    },
+    {
+        letter: "ذَ",
+        name: "Dhal with Fathah",
+        pronunciation: ["dha"],
+    },
+    {
+        letter: "ذِ",
+        name: "Dhal with Kasrah",
+        pronunciation: ["dhi"],
+    },
+    {
+        letter: "ذُ",
+        name: "Dhal with Dammah",
+        pronunciation: ["dhu"],
+    },
+    {
+        letter: "رَ",
+        name: "Ra with Fathah",
+        pronunciation: ["ra"],
+    },
+    {
+        letter: "رِ",
+        name: "Ra with Kasrah",
+        pronunciation: ["ri"],
+    },
+    {
+        letter: "رُ",
+        name: "Ra with Dammah",
+        pronunciation: ["ru"],
+    },
+    {
+        letter: "زَ",
+        name: "Zay with Fathah",
+        pronunciation: ["za"],
+    },
+    {
+        letter: "زِ",
+        name: "Zay with Kasrah",
+        pronunciation: ["zi"],
+    },
+    {
+        letter: "زُ",
+        name: "Zay with Dammah",
+        pronunciation: ["zu"],
+    },
+    {
+        letter: "سَ",
+        name: "Seen with Fathah",
+        pronunciation: ["sa"],
+    },
+    {
+        letter: "سِ",
+        name: "Seen with Kasrah",
+        pronunciation: ["si"],
+    },
+    {
+        letter: "سُ",
+        name: "Seen with Dammah",
+        pronunciation: ["su"],
+    },
+    {
+        letter: "شَ",
+        name: "Sheen with Fathah",
+        pronunciation: ["sha"],
+    },
+    {
+        letter: "شِ",
+        name: "Sheen with Kasrah",
+        pronunciation: ["shi"],
+    },
+    {
+        letter: "شُ",
+        name: "Sheen with Dammah",
+        pronunciation: ["shu"],
+    },
+    {
+        letter: "صَ",
+        name: "Sad with Fathah",
+        pronunciation: ["sa"],
+    },
+    {
+        letter: "صِ",
+        name: "Sad with Kasrah",
+        pronunciation: ["si"],
+    },
+    {
+        letter: "صُ",
+        name: "Sad with Dammah",
+        pronunciation: ["su"],
+    },
+    {
+        letter: "ضَ",
+        name: "Dad with Fathah",
+        pronunciation: ["da"],
+    },
+    {
+        letter: "ضِ",
+        name: "Dad with Kasrah",
+        pronunciation: ["di"],
+    },
+    {
+        letter: "ضُ",
+        name: "Dad with Dammah",
+        pronunciation: ["du"],
+    },
+    {
+        letter: "طَ",
+        name: "Tah with Fathah",
+        pronunciation: ["ta"],
+    },
+    {
+        letter: "طِ",
+        name: "Tah with Kasrah",
+        pronunciation: ["ti"],
+    },
+    {
+        letter: "طُ",
+        name: "Tah with Dammah",
+        pronunciation: ["tu"],
+    },
+    {
+        letter: "ظَ",
+        name: "Zah with Fathah",
+        pronunciation: ["za"],
+    },
+    {
+        letter: "ظِ",
+        name: "Zah with Kasrah",
+        pronunciation: ["zi"],
+    },
+    {
+        letter: "ظُ",
+        name: "Zah with Dammah",
+        pronunciation: ["zu"],
+    },
+    {
+        letter: "عَ",
+        name: "Ayn with Fathah",
+        pronunciation: ["a"],
+    },
+    {
+        letter: "عِ",
+        name: "Ayn with Kasrah",
+        pronunciation: ["i"],
+    },
+    {
+        letter: "عُ",
+        name: "Ayn with Dammah",
+        pronunciation: ["u"],
+    },
+    {
+        letter: "غَ",
+        name: "Ghayn with Fathah",
+        pronunciation: ["gha"],
+    },
+    {
+        letter: "غِ",
+        name: "Ghayn with Kasrah",
+        pronunciation: ["ghi"],
+    },
+    {
+        letter: "غُ",
+        name: "Ghayn with Dammah",
+        pronunciation: ["ghu"],
+    },
+    {
+        letter: "فَ",
+        name: "Fa with Fathah",
+        pronunciation: ["fa"],
+    },
+    {
+        letter: "فِ",
+        name: "Fa with Kasrah",
+        pronunciation: ["fi"],
+    },
+    {
+        letter: "فُ",
+        name: "Fa with Dammah",
+        pronunciation: ["fu"],
+    },
+    {
+        letter: "قَ",
+        name: "Qaf with Fathah",
+        pronunciation: ["qa"],
+    },
+    {
+        letter: "قِ",
+        name: "Qaf with Kasrah",
+        pronunciation: ["qi"],
+    },
+    {
+        letter: "قُ",
+        name: "Qaf with Dammah",
+        pronunciation: ["qu"],
+    },
+    {
+        letter: "كَ",
+        name: "Kaf with Fathah",
+        pronunciation: ["ka"],
+    },
+    {
+        letter: "كِ",
+        name: "Kaf with Kasrah",
+        pronunciation: ["ki"],
+    },
+    {
+        letter: "كُ",
+        name: "Kaf with Dammah",
+        pronunciation: ["ku"],
+    },
+    {
+        letter: "لَ",
+        name: "Lam with Fathah",
+        pronunciation: ["la"],
+    },
+    {
+        letter: "لِ",
+        name: "Lam with Kasrah",
+        pronunciation: ["li"],
+    },
+    {
+        letter: "لُ",
+        name: "Lam with Dammah",
+        pronunciation: ["lu"],
+    },
+    {
+        letter: "مَ",
+        name: "Meem with Fathah",
+        pronunciation: ["ma"],
+    },
+    {
+        letter: "مِ",
+        name: "Meem with Kasrah",
+        pronunciation: ["mi"],
+    },
+    {
+        letter: "مُ",
+        name: "Meem with Dammah",
+        pronunciation: ["mu"],
+    },
+    {
+        letter: "نَ",
+        name: "Noon with Fathah",
+        pronunciation: ["na"],
+    },
+    {
+        letter: "نِ",
+        name: "Noon with Kasrah",
+        pronunciation: ["ni"],
+    },
+    {
+        letter: "نُ",
+        name: "Noon with Dammah",
+        pronunciation: ["nu"],
+    },
+    {
+        letter: "هَ",
+        name: "Ha with Fathah",
+        pronunciation: ["ha"],
+    },
+    {
+        letter: "هِ",
+        name: "Ha with Kasrah",
+        pronunciation: ["hi"],
+    },
+    {
+        letter: "هُ",
+        name: "Ha with Dammah",
+        pronunciation: ["hu"],
+    },
+    {
+        letter: "وَ",
+        name: "Waw with Fathah",
+        pronunciation: ["wa"],
+    },
+    {
+        letter: "وِ",
+        name: "Waw with Kasrah",
+        pronunciation: ["wi"],
+    },
+    {
+        letter: "وُ",
+        name: "Waw with Dammah",
+        pronunciation: ["wu"],
+    },
+    {
+        letter: "يَ",
+        name: "Ya with Fathah",
+        pronunciation: ["ya"],
+    },
+    {
+        letter: "يِ",
+        name: "Ya with Kasrah",
+        pronunciation: ["yi"],
+    },
+    {
+        letter: "يُ",
+        name: "Ya with Dammah",
+        pronunciation: ["yu"],
+    },
+];
