8b8d74b17dc538d7bf46ce1005ce63c1988e4840
AbuIbrahim <AntonRoumelR>
2025-03-02 15:37:25 +0800

    Initial commit for upload

---
+++ src/letters/basic-letters.js
@@ -0,0 +1,152 @@
+export const arabicLetters = [
+    {
+      letter: "ا",
+      name: "Alif",
+      pronunciation: ["a", "alif"],
+    },
+    {
+      letter: "ب",
+      name: "Ba",
+      pronunciation: ["b", "ba"],
+    },
+    {
+      letter: "ت",
+      name: "Ta",
+      pronunciation: "t",
+    },
+    {
+      letter: "ث",
+      name: "Tha",
+      pronunciations: ["tha", "th"],
+    },
+    {
+        letter: "ج",
+        name: "Jeem",
+        pronunciations: ["jeem", "jem", "jim"],
+    },
+    {
+        letter: "ح",
+        name: "Hah",
+        pronunciations: ["hah"],
+    },
+    {
+        letter: "خ",
+        name: "Kha",
+        pronunciations: ["kha","kh"],
+    },
+    {
+        letter: "د",
+        name: "Dha",
+        pronunciations: ["dha","dh"],
+    },
+    {
+        letter: "ذ",
+        name: "Dhal",
+        pronunciations: ["dhal"],
+    },
+    {
+        letter: "ر",
+        name: "Ra",
+        pronunciations: ["ra", "r"],
+    },
+    {
+        letter: "ز",
+        name: "Zay",
+        pronunciations: ["zay", "z"],
+    },
+    {
+        letter: "س",
+        name: "Seen",
+        pronunciations: ["seen", "sin"],
+    },
+    {
+        letter: "س",
+        name: "Seen",
+        pronunciations: ["seen", "sin"],
+    },
+    {
+        letter: "ش",
+        name: "Sheen",
+        pronunciations: ["sheen", "shin"],
+    },
+    {
+        letter: "ص",
+        name: "Sodh",
+        pronunciations: ["sodh", "sod"],
+    },
+    {
+        letter: "ض",
+        name: "Dodh",
+        pronunciations: ["dodh", "dod"],
+    },
+    {
+        letter: "ط",
+        name: "Toah",
+        pronunciations: ["toah", "toa"],
+    },
+    {
+        letter: "ظ",
+        name: "Doah",
+        pronunciations: ["doah", "doa"],
+    },
+    {
+        letter: "ع",
+        name: "Aiyn",
+        pronunciations: ["ayin", "aiyn", "ayn", "ain"],
+    },
+    {
+        letter: "غ",
+        name: "Ghayin",
+        pronunciations: ["ghayin", "ghaiyn", "ghain", "ghayn"],
+    },
+    {
+        letter: "ف",
+        name: "Fa",
+        pronunciations: ["fa", "fah"],
+    },
+    {
+        letter: "ق",
+        name: "Qaf",
+        pronunciations: ["qaf", "qa"],
+    },
+    {
+        letter: "ك",
+        name: "Kaf",
+        pronunciations: ["kaf", "ka"],
+    },
+    {
+        letter: "ل",
+        name: "Lam",
+        pronunciations: ["lam", "la"],
+    },
+    {
+        letter: "م",
+        name: "Meem",
+        pronunciations: ["meem", "mim"],
+    },
+    {
+        letter: "ن",
+        name: "Noon",
+        pronunciations: ["noon", "nun"],
+    },
+    {
+        letter: "ه",
+        name: "Ha",
+        pronunciations: ["ha", "h"],
+    },
+    {
+        letter: "و",
+        name: "Waw",
+        pronunciations: ["waw", "wow"],
+    },
+    {
+        letter: "ي",
+        name: "Ya",
+        pronunciations: ["ya"],
+    },
+    {
+        letter: "ء",
+        name: "Hamza",
+        pronunciations: ["hamza"],
+    },
+  ];
---
+++ src/letters/tanween-letters.js
@@ -0,0 +1,432 @@
+export const arabicLettersTanween = [
+  {
+    letter: "اَ",
+    name: "Alif with Fatha",
+    pronunciation: ["a"],
+  },
+  {
+    letter: "اِ",
+    name: "Alif with Kasrah",
+    pronunciation: ["i"],
+  },
+  {
+    letter: "اُ",
+    name: "Alif with Dammah",
+    pronunciation: ["u"],
+  },
+  {
+    letter: "اً",
+    name: "Alif with Tanween Fathatain",
+    pronunciation: ["an"],
+  },
+  {
+    letter: "اٍ",
+    name: "Alif with Tanween Kasratain",
+    pronunciation: ["in"],
+  },
+  {
+    letter: "ًاٌ",
+    name: "Alif with Tanween Dammatain",
+    pronunciation: ["un"],
+  },
+  {
+    letter: "بً",
+    name: "Ba with Tanween Fathatain",
+    pronunciation: ["ban"],
+  },
+  {
+    letter: "بٌ",
+    name: "Ba with Tanween Dammatain",
+    pronunciation: ["bun"],
+  },
+  {
+    letter: "بٍ",
+    name: "Ba with Tanween Kasratain",
+    pronunciation: ["bin"],
+  },
+  {
+    letter: "تً",
+    name: "Ta with Tanween Fathatain",
+    pronunciation: ["tan"],
+  },
+  {
+    letter: "تٌ",
+    name: "Ta with Tanween Dammatain",
+    pronunciation: ["tun"],
+  },
+  {
+    letter: "تٍ",
+    name: "Ta with Tanween Kasratain",
+    pronunciation: ["tin"],
+  },
+  {
+    letter: "ثً",
+    name: "Tha with Tanween Fathatain",
+    pronunciation: ["than"],
+  },
+  {
+    letter: "ثٌ",
+    name: "Tha with Tanween Dammatain",
+    pronunciation: ["thun"],
+  },
+  {
+    letter: "ثٍ",
+    name: "Tha with Tanween Kasratain",
+    pronunciation: ["thin"],
+  },
+  {
+    letter: "جً",
+    name: "Jeem with Tanween Fathahtain",
+    pronunciation: ["jan"],
+  },
+  {
+    letter: "جٌ",
+    name: "Jeem with Tanween Dammahtain",
+    pronunciation: ["jun"],
+  },
+  {
+    letter: "جٍ",
+    name: "Jeem with Tanween Kasrahtain",
+    pronunciation: ["jin"],
+  },
+  {
+    letter: "حً",
+    name: "Hah with Tanween Fathahtain",
+    pronunciation: ["han"],
+  },
+  {
+    letter: "حٌ",
+    name: "Hah with Tanween Dammahtain",
+    pronunciation: ["hun"],
+  },
+  {
+    letter: "حٍ",
+    name: "Hah with Tanween Kasrahtain",
+    pronunciation: ["hin"],
+  },
+  {
+    letter: "خً",
+    name: "Kha with Tanween Fathahtain",
+    pronunciation: ["khan"],
+  },
+  {
+    letter: "خٌ",
+    name: "Kha with Tanween Dammahtain",
+    pronunciation: ["khun"],
+  },
+  {
+    letter: "خٍ",
+    name: "Kha with Tanween Kasrahtain",
+    pronunciation: ["khin"],
+  },
+  {
+    letter: "دً",
+    name: "Dha with Tanween Fathahtain",
+    pronunciation: ["dan", "dhan"],
+  },
+  {
+    letter: "دٌ",
+    name: "Dha with Tanween Dammahtain",
+    pronunciation: ["dun", "dhun"],
+  },
+  {
+    letter: "دٍ",
+    name: "Dha with Tanween Kasrahtain",
+    pronunciation: ["din", "dhin"],
+  },
+  {
+    letter: "ذً",
+    name: "Dhal with Tanween Fathahtain",
+    pronunciation: ["dhaan"],
+  },
+  {
+    letter: "ذٌ",
+    name: "Dhal with Tanween Dammahtain",
+    pronunciation: ["dhun"],
+  },
+  {
+    letter: "ذٍ",
+    name: "Dhal with Tanween Kasrahtain",
+    pronunciation: ["dhin"],
+  },
+  {
+    letter: "رً",
+    name: "Ra with Tanween Fathahtain",
+    pronunciation: ["ran"],
+  },
+  {
+    letter: "رٌ",
+    name: "Ra with Tanween Dammahtain",
+    pronunciation: ["run"],
+  },
+  {
+    letter: "رٍ",
+    name: "Ra with Tanween Kasrahtain",
+    pronunciation: ["rin"],
+  },
+  {
+    letter: "زً",
+    name: "Zay with Tanween Fathahtain",
+    pronunciation: ["zan"],
+  },
+  {
+    letter: "زٌ",
+    name: "Zay with Tanween Dammahtain",
+    pronunciation: ["zun"],
+  },
+  {
+    letter: "زٍ",
+    name: "Zay with Tanween Kasrahtain",
+    pronunciation: ["zin"],
+  },
+  {
+    letter: "سً",
+    name: "Seen with Tanween Fathahtain",
+    pronunciation: ["san"],
+  },
+  {
+    letter: "سٌ",
+    name: "Seen with Tanween Dammahtain",
+    pronunciation: ["sun"],
+  },
+  {
+    letter: "سٍ",
+    name: "Seen with Tanween Kasrahtain",
+    pronunciation: ["sin"],
+  },
+  {
+    letter: "شً",
+    name: "Sheen with Tanween Fathahtain",
+    pronunciation: ["shan"],
+  },
+  {
+    letter: "شٌ",
+    name: "Sheen with Tanween Dammahtain",
+    pronunciation: ["shun"],
+  },
+  {
+    letter: "شٍ",
+    name: "Sheen with Tanween Kasrahtain",
+    pronunciation: ["shin"],
+  },
+  {
+    letter: "صً",
+    name: "Sodh with Tanween Fathahtain",
+    pronunciation: ["soan"],
+  },
+  {
+    letter: "صٌ",
+    name: "Sodh with Tanween Dammahtain",
+    pronunciation: ["soun"],
+  },
+  {
+    letter: "صٍ",
+    name: "Sodh with Tanween Kasrahtain",
+    pronunciation: ["soin"],
+  },
+  {
+    letter: "ضً",
+    name: "Dodh with Tanween Fathahtain",
+    pronunciation: ["dhoan", "doan"],
+  },
+  {
+    letter: "ضٌ",
+    name: "Dodh with Tanween Dammahtain",
+    pronunciation: ["dhoun", "dhun"],
+  },
+  {
+    letter: "ضٍ",
+    name: "Dodh with Tanween Kasrahtain",
+    pronunciation: ["dhoin", "duin"],
+  },
+  {
+    letter: "طً",
+    name: "Toah with Tanween Fathahtain",
+    pronunciation: ["toan"],
+  },
+  {
+    letter: "طٌ",
+    name: "Toah with Tanween Dammahtain",
+    pronunciation: ["toun"],
+  },
+  {
+    letter: "طٍ",
+    name: "Toah with Tanween Kasrahtain",
+    pronunciation: ["toin"],
+  },
+  {
+    letter: "ظً",
+    name: "Doah with Tanween Fathahtain",
+    pronunciation: ["dhoan", "thoan"],
+  },
+  {
+    letter: "ظٌ",
+    name: "Doah with Tanween Dammahtain",
+    pronunciation: ["dhoun", "thoun"],
+  },
+  {
+    letter: "ظٍ",
+    name: "Doah with Tanween Kasrahtain",
+    pronunciation: ["doain"],
+  },
+  {
+    letter: "عً",
+    name: "Aiyn with Tanween Fathahtain",
+    pronunciation: ["an", "a'an"],
+  },
+  {
+    letter: "عٌ",
+    name: "Aiyn with Tanween Dammahtain",
+    pronunciation: ["u'un", "un"],
+  },
+  {
+    letter: "عٍ",
+    name: "Aiyn with Tanween Kasrahtain",
+    pronunciation: ["in", "i'in"],
+  },
+  {
+    letter: "غً",
+    name: "Ghayin with Tanween Fathahtain",
+    pronunciation: ["gha'an", "ghan"],
+  },
+  {
+    letter: "غٌ",
+    name: "Ghayin with Tanween Dammahtain",
+    pronunciation: ["ghu'un", "ghun"],
+  },
+  {
+    letter: "غٍ",
+    name: "Ghayin with Tanween Kasrahtain",
+    pronunciation: ["ghi'in", "ghin"],
+  },
+  {
+    letter: "فً",
+    name: "Fa with Tanween Fathahtain",
+    pronunciation: ["fan"],
+  },
+  {
+    letter: "فٌ",
+    name: "Fa with Tanween Dammahtain",
+    pronunciation: ["fun"],
+  },
+  {
+    letter: "فٍ",
+    name: "Fa with Tanween Kasrahtain",
+    pronunciation: ["fin"],
+  },
+  {
+    letter: "قً",
+    name: "Qaf with Tanween Fathahtain",
+    pronunciation: ["qan"],
+  },
+  {
+    letter: "قٌ",
+    name: "Qaf with Tanween Dammahtain",
+    pronunciation: ["qun"],
+  },
+  {
+    letter: "قٍ",
+    name: "Qaf with Tanween Kasrahtain",
+    pronunciation: ["qin"],
+  },
+  {
+    letter: "كً",
+    name: "Kaf with Tanween Fathahtain",
+    pronunciation: ["kan"],
+  },
+  {
+    letter: "كٌ",
+    name: "Kaf with Tanween Dammahtain",
+    pronunciation: ["kun"],
+  },
+  {
+    letter: "كٍ",
+    name: "Kaf with Tanween Kasrahtain",
+    pronunciation: ["kin"],
+  },
+  {
+    letter: "لً",
+    name: "Lam with Tanween Fathahtain",
+    pronunciation: ["lan"],
+  },
+  {
+    letter: "لٌ",
+    name: "Lam with Tanween Dammahtain",
+    pronunciation: ["lun"],
+  },
+  {
+    letter: "لٍ",
+    name: "Lam with Tanween Kasrahtain",
+    pronunciation: ["lin"],
+  },
+  {
+    letter: "مً",
+    name: "Meem with Tanween Fathahtain",
+    pronunciation: ["man"],
+  },
+  {
+    letter: "مٌ",
+    name: "Meem with Tanween Dammahtain",
+    pronunciation: ["mun"],
+  },
+  {
+    letter: "مٍ",
+    name: "Meem with Tanween Kasrahtain",
+    pronunciation: ["min"],
+  },
+  {
+    letter: "نً",
+    name: "Noon with Tanween Fathahtain",
+    pronunciation: ["nan"],
+  },
+  {
+    letter: "نٌ",
+    name: "Noon with Tanween Dammahtain",
+    pronunciation: ["nun"],
+  },
+  {
+    letter: "نٍ",
+    name: "Noon with Tanween Kasrahtain",
+    pronunciation: ["nin"],
+  },
+  {
+    letter: "هً",
+    name: "Ha with Tanween Fathahtain",
+    pronunciation: ["han"],
+  },
+  {
+    letter: "هٌ",
+    name: "Ha with Tanween Dammahtain",
+    pronunciation: ["hun"],
+  },
+  {
+    letter: "هٍ",
+    name: "Ha with Tanween Kasrahtain",
+    pronunciation: ["hin"],
+  },
+  {
+    letter: "وً",
+    name: "Waw with Tanween Fathahtain",
+    pronunciation: ["wan"],
+  },
+  {
+    letter: "وٌ",
+    name: "Waw with Tanween Dammahtain",
+    pronunciation: ["wun"],
+  },
+  {
+    letter: "يً",
+    name: "Ya with Tanween Fathahtain",
+    pronunciation: ["yan"],
+  },
+  {
+    letter: "يٍ",
+    name: "Ya with Tanween Kasrahtain",
+    pronunciation: ["yin"],
+  },
+  {
+    letter: "يٌ",
+    name: "Ya with Tanween Dammahtain",
+    pronunciation: ["yun"],
+  },
+];
